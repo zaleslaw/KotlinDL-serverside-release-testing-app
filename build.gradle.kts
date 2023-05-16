b71ab48f0318d02880be3529240ff10f79708a8e
zaleslaw <zaleslaw.sin@gmail.com>
2023-05-16 14:11:15 +0200

    Moved to the release 0.5.2 version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.0"
-    id("org.jetbrains.kotlinx.dataframe") version "0.10.0"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 group = "com.zaleslaw.kotlindl.serverside.release-testing-app"
@@ -11,10 +11,10 @@ repositories {
 }
 
 dependencies {
-    implementation ("org.jetbrains.kotlinx:kotlin-deeplearning-tensorflow:0.5.2-alpha-1")
-    implementation ("org.jetbrains.kotlinx:kotlin-deeplearning-dataset:0.5.2-alpha-1")
-    implementation ("org.jetbrains.kotlinx:kotlin-deeplearning-onnx:0.5.2-alpha-1")
-    implementation ("org.jetbrains.kotlinx:kotlin-deeplearning-visualization:0.5.2-alpha-1")
+    implementation ("org.jetbrains.kotlinx:kotlin-deeplearning-tensorflow:0.5.2")
+    implementation ("org.jetbrains.kotlinx:kotlin-deeplearning-dataset:0.5.2")
+    implementation ("org.jetbrains.kotlinx:kotlin-deeplearning-onnx:0.5.2")
+    implementation ("org.jetbrains.kotlinx:kotlin-deeplearning-visualization:0.5.2")
     implementation ("org.apache.logging.log4j:log4j-api:2.17.2")
     implementation ("org.apache.logging.log4j:log4j-core:2.17.2")
     implementation ("org.apache.logging.log4j:log4j-slf4j-impl:2.17.2")
@@ -32,6 +32,15 @@ tasks.test {
     // set heap size for the test JVM(s)
     minHeapSize = "1024m"
     maxHeapSize = "8g"
+}
+
+tasks{
+    shadowJar {
+        manifest {
+            attributes(Pair("Main-Class", "MainKt"))
+            attributes(Pair("Implementation-Version", "1.15"))
+        }
+    }
 }
 
 kotlin {
